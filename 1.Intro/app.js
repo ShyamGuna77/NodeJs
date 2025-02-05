2ef765fa070a1934c7ddd50513d1e3b5ea605a08
Shyam <gshyamprasad2001@gmail.com>
2025-02-05 21:57:14 +0530

    Basics

--- 1.Intro/app.js
+++ 1.Intro/app.js
@@ -1,1 +1,10 @@
+  require("./script.js")
+  const obj = require('./sum.js')
+  let a = 10 ;
+  let b = 30;
+  obj.Calculate(a,b);
+  console.log(obj.x);
+
+
 console.log("Hi Luffy");
+
---
+++ 1.Intro/sum.js
@@ -0,0 +1,11 @@
+function Calculate(a,b){
+    const sum = a+b;
+    console.log(sum);
+}
+
+var x = "Hi Zoro"
+
+module.exports = {
+    x:x,
+    Calculate:Calculate
+}
