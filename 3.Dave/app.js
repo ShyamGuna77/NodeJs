d5c609b7bf4774341069c8a7ad96d3dafc454e42
Shyam <gshyamprasad2001@gmail.com>
2025-02-25 12:16:29 +0530

    init

---
+++ 3.Dave/app.js
@@ -0,0 +1,55 @@
+
+const fs = require("fs")
+fs.writeFile("example.txt", "Hello, world!  this is fs module ffuufufu", "utf8", (err) => {
+  if (err) {
+    console.error("Error writing file:", err);
+    return;
+  }
+  console.log("File written successfully");
+});
+
+
+fs.rename("example.txt", "renamed.txt", (err) => {
+  if (err) {
+    console.error("Error renaming file:", err);
+    return;
+  }
+  console.log("File renamed successfully");
+});
+
+
+
+
+fs.appendFile("log.txt", "New log entry\n", "utf8", (err) => {
+  if (err) {
+    console.error("Error appending to file:", err);
+    return;
+  }
+  console.log("Content appended successfully!");
+});
+
+const logEntry = `Log entry at ${new Date().toISOString()}\n`;
+
+fs.appendFile("log.txt", logEntry, "utf8", (err) => {
+  if (err) {
+    console.error("Error writing log:", err);
+    return;
+  }
+  console.log("Log entry added:", logEntry);
+});
+
+
+
+const logData = {
+  timestamp: new Date().toISOString(),
+  level: "INFO",
+  message: "User logged in",
+};
+
+fs.appendFile("logs.json", JSON.stringify(logData) + ",\n", "utf8", (err) => {
+  if (err) {
+    console.error("Error appending JSON data:", err);
+    return;
+  }
+  console.log("JSON log appended:", logData);
+});
