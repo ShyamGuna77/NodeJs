caa0a4056d7be0a6ac53e31523398882793e15b5
Shyam <gshyamprasad2001@gmail.com>
2025-03-01 07:40:56 +0530

    middi

--- 4.Headers/app.js
+++ 4.Headers/app.js
@@ -3,29 +3,38 @@ const fs = require('fs')
 const express = require("express")
 const app = express()
 
-app.get("/",(req,res)=>{
-    console.log("first request 1 here");
-    res.send("response 1")
-})
+// app.get("/",(req,res)=>{
+//     console.log("first request 1 here");
+//     res.send("response 1")
+// })
 
-app.get(
-  "/user",
-  (req, res, next) => {
-    console.log("Second request 1 here");
-    next();
-    res.send("usr Response 2");
-  },
-  (req, res,next) => {
-    console.log("Third request 1 here");
-    res.send("user Response 3");
-    next();
-  },
-  (req, res,next) => {
-    console.log("fourth request 1 here");
-    res.send("user Response 4");
-    next();
-  }
-);
+app.use((req, res, next) => {
+  console.log(`${req.method} request to ${req.url}`);
+  next(); // Pass control to the next middleware
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+// app.get(
+//   "/user",
+//   (req, res, next) => {
+//     console.log("Second request 1 here");
+//     next();
+//     res.send("usr Response 2");
+//   },
+//   (req, res,next) => {
+//     console.log("Third request 1 here");
+//     res.send("user Response 3");
+//     next();
+//   },
+//   (req, res,next) => {
+//     console.log("fourth request 1 here");
+//     res.send("user Response 4");
+//     next();
+//   }
+// );
 
 
 app.listen(3000,()=>{
